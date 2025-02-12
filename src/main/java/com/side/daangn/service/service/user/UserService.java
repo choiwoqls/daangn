65a80b513c9a6cb1e4e44370a1fbb49e28cd10c7
ChoiJB <byin0209@naver.com>
2025-02-13 01:21:34 +0900

    feat : userService -> existsById 추가

--- src/main/java/com/side/daangn/service/service/user/UserService.java
+++ src/main/java/com/side/daangn/service/service/user/UserService.java
@@ -1,6 +1,7 @@
 package com.side.daangn.service.service.user;
 
 import com.side.daangn.dto.request.auth.SignUpDTO;
+import com.side.daangn.dto.response.user.SearchPageDTO;
 import com.side.daangn.dto.response.user.UserDTO;
 
 import java.util.UUID;
@@ -14,6 +15,8 @@ public interface UserService {
     UserDTO findById(UUID id);
 
     boolean existsByEmail(String email);
+
+    boolean existsById(UUID id);
 
     String logout();
 
