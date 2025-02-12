3850efb132d689583fb12e907f6d90976836cbcc
ChoiJB <byin0209@naver.com>
2025-02-12 22:56:15 +0900

    fix : sign-upDTO code 추가

--- src/main/java/com/side/daangn/dto/request/SignUpDTO.java
+++ src/main/java/com/side/daangn/dto/request/SignUpDTO.java
@@ -22,6 +22,9 @@ public class SignUpDTO {
     @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "8자 이상 알파벳, 숫자, 특수문자를 포함한 비밀번호를 입력해주세요.")
     private String password;
 
+    @NotBlank(message = "코드를 입력해주세요")
+    private String code;
+
 
 
 
