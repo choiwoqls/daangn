33a3477229ceb063d0f2007cfc4b8e20ac081e0e
ChoiJB <byin0209@naver.com>
2025-02-12 22:53:04 +0900

    feat : 스케줄러 추가

---
+++ src/main/java/com/side/daangn/scheduling/ScheduledTasks.java
@@ -0,0 +1,59 @@
+package com.side.daangn.scheduling;
+
+import com.side.daangn.entitiy.product.Search;
+import com.side.daangn.service.service.product.SearchService;
+import com.side.daangn.util.RedisUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Set;
+
+@Component
+
+public class ScheduledTasks {
+
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+	private final RedisUtil redisUtil;
+
+	private final SearchService searchService;
+
+    public ScheduledTasks(RedisUtil redisUtil, SearchService searchService) {
+        this.redisUtil = redisUtil;
+        this.searchService = searchService;
+    }
+
+    //매일 자정에 함수 실행
+    //검색어 DB에 업데이트.
+	@Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
+	public void updateSearchPerDay() {
+        log.info("Cron Check{}", dateFormat.format(new Date()));
+		Set<String> keys = redisUtil.getKeysByPattern("search_*");
+		if (keys != null && !keys.isEmpty()){
+			for(String key : keys){
+				String search = key.split("_")[1];
+				long count = Long.parseLong(redisUtil.getToken(key));
+				Search searchEntity = searchService.findBySearch(search);
+				if(searchEntity!= null){
+					searchService.searchPlus(search, count);
+				}else{
+					searchEntity = new Search();
+					searchEntity.setSearch(search);
+					searchEntity.setCount(count);
+					searchService.save(searchEntity);
+				}
+			}
+		}
+		redisUtil.deleteKeys(keys);
+	}
+//    @Scheduled(fixedRate = 5000)
+//	public void reportCurrentTime() {
+//		log.info("The time is now {}", dateFormat.format(new Date()));
+//	}
+}
