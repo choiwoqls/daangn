91bc9169d3f2dd468b6eb1d33dccaf563bb37a6e
ChoiJB <byin0209@naver.com>
2025-02-19 00:35:53 +0900

    fix : 이메일 전송 메소드 수정 (매개변수 추가)

--- src/main/java/com/side/daangn/util/MailUtil.java
+++ src/main/java/com/side/daangn/util/MailUtil.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,12 +12,13 @@ public class MailUtil {
 
     private final JavaMailSender javaMailSender;
 
-    public void sendMailReject(String email, String code){
+    @Transactional
+    public void sendMailReject(String email,String title, String text){
         try {
             SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
             simpleMailMessage.setTo(email);
-            simpleMailMessage.setSubject("이메일 인증 - Daangn");
-            simpleMailMessage.setText("인증번호 : " + code);
+            simpleMailMessage.setSubject(title);
+            simpleMailMessage.setText(text);
             javaMailSender.send(simpleMailMessage);
         }catch (Exception e){
             System.out.println("Exception - MailUtil : sendMailReject");
