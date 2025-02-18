d8e2c0febada5d6746612e61ec1fc990d7d1e511
ChoiJB <byin0209@naver.com>
2025-02-19 00:33:36 +0900

    feat : 임시 비밀번호 생성

--- src/main/java/com/side/daangn/util/CodeUtil.java
+++ src/main/java/com/side/daangn/util/CodeUtil.java
@@ -3,7 +3,7 @@ package com.side.daangn.util;
 import java.security.SecureRandom;
 
 public class CodeUtil {
-    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final String CHARACTERS = "abcdefghijklmnopqrstuvwxyz0123456789";
     private static final SecureRandom RANDOM = new SecureRandom();
 
     public static String generate() {
@@ -13,5 +13,12 @@ public class CodeUtil {
             result.append(CHARACTERS.charAt(index));
         }
         return result.toString();
+    }
+    public static String generateRandomString(int length) {
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(CHARACTERS.charAt(RANDOM.nextInt(CHARACTERS.length())));
+        }
+        return sb.toString();
     }
 }
