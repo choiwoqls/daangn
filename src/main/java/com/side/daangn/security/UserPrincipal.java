c8d9a51f9be81a4e0aa91bbb48f9dddf1b45df7f
ChoiJB <byin0209@naver.com>
2025-02-19 00:42:17 +0900

    feat : UserPrincipal 에 attribute 추가(kakao login 시  유저 정보 담아옴)

--- src/main/java/com/side/daangn/security/UserPrincipal.java
+++ src/main/java/com/side/daangn/security/UserPrincipal.java
@@ -8,15 +8,17 @@ import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Getter
 @Setter
-@AllArgsConstructor
-public class UserPrincipal implements UserDetails {
+//@AllArgsConstructor
+public class UserPrincipal implements UserDetails, OAuth2User {
 
     private UUID id;
     private String email;
@@ -24,12 +26,44 @@ public class UserPrincipal implements UserDetails {
     private String password;
     private String name;
 
+    private Map<String, Object> attributes;
+
+    public UserPrincipal(UUID id, String email, String password, String name) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+
+    public UserPrincipal(UUID id, String email, String password, String name, Map<String, Object> attributes) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.attributes = attributes;
+    }
+
+
      public static UserPrincipal create(User user) {
         return new UserPrincipal(
                 user.getId(),
                 user.getEmail(),
                 user.getPassword(),
                 user.getName());
+    }
+
+     public static UserPrincipal create(User user, Map<String, Object> attributes) {
+        return new UserPrincipal(
+                user.getId(),
+                user.getEmail(),
+                user.getPassword(),
+                user.getName(),
+                attributes);
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
     }
 
     @Override
