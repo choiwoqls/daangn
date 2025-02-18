e4dd36b49da7e01054c49de6c9b1f030a57c0850
ChoiJB <byin0209@naver.com>
2025-02-19 00:34:59 +0900

    style : 줄바꿈

--- src/main/java/com/side/daangn/security/JwtAuthenticationFilter.java
+++ src/main/java/com/side/daangn/security/JwtAuthenticationFilter.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.util.StringUtils;
@@ -49,6 +50,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                             null,
                             userDetails.getAuthorities());
                     SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
                 }
             } catch (Exception e) {
                 logger.error("Could not set user authentication in security context", e);
