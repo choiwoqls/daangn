a93dfc30cea171638f81b8153f7abcbcff6eb239
ChoiJB <byin0209@naver.com>
2025-02-19 00:37:39 +0900

    feat : 빈 multipartfile 객체 생성 클래스 추가

---
+++ src/main/java/com/side/daangn/util/EmptyMultipartFile.java
@@ -0,0 +1,49 @@
+package com.side.daangn.util;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class EmptyMultipartFile implements MultipartFile {
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getOriginalFilename() {
+        return null;
+    }
+
+    @Override
+    public String getContentType() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return true;
+    }
+
+    @Override
+    public long getSize() {
+        return 0;
+    }
+
+    @Override
+    public byte[] getBytes() throws IOException {
+        return new byte[0];
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+        return null;
+    }
+
+    @Override
+    public void transferTo(File dest) throws IOException, IllegalStateException {
+
+    }
+}
