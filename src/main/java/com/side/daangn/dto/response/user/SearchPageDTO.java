3b5bf139dd1be535eb62962947e06043058004e9
ChoiJB <byin0209@naver.com>
2025-02-25 22:08:04 +0900

    fix : product page response elementCount 삭제

--- src/main/java/com/side/daangn/dto/response/user/SearchPageDTO.java
+++ src/main/java/com/side/daangn/dto/response/user/SearchPageDTO.java
@@ -18,14 +18,12 @@ public class SearchPageDTO {
     private int pageNum;
     private int pageSize;
     private int maxPage;
-    private Long elementCount;
     private List<ProductResponseDTO> products;
 
-    public SearchPageDTO(int pageNum, int pageSize,int maxPage, Long elementCount, List<ProductResponseDTO> products){
+    public SearchPageDTO(int pageNum, int pageSize,int maxPage, List<ProductResponseDTO> products){
         this.pageNum = pageNum + 1;
         this.pageSize = pageSize;
         this.maxPage = maxPage;
-        this.elementCount = elementCount;
         this.products = products;
     }
 }
